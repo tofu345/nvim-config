16450db20b7fcdae7ce8778ec6537d8ea28a0136
tofu345 <shammahayotunde1@gmail.com>
2023-11-14 21:45:24 +0000

    x

--- lua/tofu345/remap.lua
+++ lua/tofu345/remap.lua
@@ -51,6 +51,8 @@ vim.cmd([[
     vnoremap <leader>R "hy:%s/\M<C-r>h//gI<left><left><left>
 ]])
 
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<A-v>", "<C-v>")
@@ -58,21 +60,6 @@ vim.keymap.set("n", "<A-v>", "<C-v>")
 -- Trouble.lua
 vim.keymap.set("n", "<leader>xx", function()
 	require("trouble").toggle()
-end)
-vim.keymap.set("n", "<leader>xw", function()
-	require("trouble").toggle("workspace_diagnostics")
-end)
-vim.keymap.set("n", "<leader>xd", function()
-	require("trouble").toggle("document_diagnostics")
-end)
-vim.keymap.set("n", "<leader>xq", function()
-	require("trouble").toggle("quickfix")
-end)
-vim.keymap.set("n", "<leader>xl", function()
-	require("trouble").toggle("loclist")
-end)
-vim.keymap.set("n", "gR", function()
-	require("trouble").toggle("lsp_references")
 end)
 
 -- LuaSnip
