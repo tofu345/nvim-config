cf15568b836e036afd37cfa8adc6f73d70e1ea94
tofu345 <shammahayotunde1@gmail.com>
2023-11-21 00:12:02 +0000

    disable italics

--- lua/tofu345/packer.lua
+++ lua/tofu345/packer.lua
@@ -30,9 +30,6 @@ return require("packer").startup(function(use)
 	use({
 		"rose-pine/neovim",
 		as = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine-main")
-		end,
 	})
 
 	use("mhartington/formatter.nvim")
