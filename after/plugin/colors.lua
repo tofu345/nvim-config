56c80baee90aab782f685b29127074b9b66a3fdb
tofu345 <shammahayotunde1@gmail.com>
2023-11-21 00:15:44 +0000

    x

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,13 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-main"
-	vim.cmd.colorscheme(color)
-
 	-- Make background transparent
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
+
+require("rose-pine").setup({
+	disable_italics = true,
+})
+
+vim.cmd("colorscheme rose-pine")
 
 ColorMyPencils()
