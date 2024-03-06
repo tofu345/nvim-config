480c87f4c5ddc2282c72c0ed176712e094efbf18
tofu345 <shammahayotunde1@gmail.com>
2024-03-06 16:28:08 +0000

    x

--- lua/tofu345/packer.lua
+++ lua/tofu345/packer.lua
@@ -16,6 +16,9 @@ return require("packer").startup(function(use)
 	use({
 		"folke/trouble.nvim",
 		requires = { { "nvim-tree/nvim-web-devicons" } },
+		config = function()
+			require("trouble").setup()
+		end,
 	})
 
 	-- snippets
