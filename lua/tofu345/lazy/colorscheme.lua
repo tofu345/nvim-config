e9cd9886d58aa7f46afdfb274710dfa2a8f5d099
tofu345 <shammahayotunde1@gmail.com>
2024-09-26 01:31:17 +0100

    colorscheme

--- lua/tofu345/lazy/colorscheme.lua
+++ lua/tofu345/lazy/colorscheme.lua
@@ -2,15 +2,8 @@ return {
 	"rktjmp/lush.nvim",
 	dependencies = {
 		"rose-pine/neovim",
-		"metalelf0/jellybeans-nvim",
 	},
 	config = function()
-		function ColorMyPencils()
-			-- Make background transparent
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		end
-
 		require("rose-pine").setup({
 			styles = {
 				italic = false,
@@ -18,8 +11,9 @@ return {
 		})
 
 		vim.cmd("colorscheme rose-pine")
-		-- vim.cmd("colorscheme jellybeans-nvim")
 
-		ColorMyPencils()
+		-- Make background transparent
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end,
 }
