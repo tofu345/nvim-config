400569efb0030d52c1752a8fee929a9a24888e8b
tofu345 <shammahayotunde1@gmail.com>
2024-10-17 15:17:52 +0100

    Paste images into markdown files, config colorscheme, auto set spell on markdown files

---
+++ lua/tofu345/lazy/clipboard-image.lua
@@ -0,0 +1,11 @@
+return {
+	"ekickx/clipboard-image.nvim",
+	ft = "markdown",
+	opts = {
+		markdown = {
+			img_dir = { "images" },
+			img_dir_txt = "./images",
+			affix = "![[%s]]",
+		},
+	},
+}
