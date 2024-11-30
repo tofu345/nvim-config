556e94440d276307d44221f5d5bb63bef16aa1c2
tofu345 <shammahayotunde1@gmail.com>
2024-11-30 13:17:26 +0000

    oil keybinds, fixed markdown ftplugin errors

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,10 +1,10 @@
--- vim.bo applies to current buffer only
+-- NOTE: vim.bo applies to current buffer only
 
-vim.bo.spell = true
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
+vim.bo.tw = 79 -- auto break
 
-vim.bo.autochdir = true -- for clipboard-image.nvim to avoid putting image in wrong folder
-
-vim.bo.tw = 80
+-- these must be global
+vim.opt.spell = true
+vim.opt.autochdir = true -- for clipboard-image.nvim to avoid putting image in wrong folder
--- lua/tofu345/lazy/oil.lua
+++ lua/tofu345/lazy/oil.lua
@@ -7,7 +7,29 @@ return {
 		view_options = {
 			show_hidden = true, -- Show files and directories that start with "."
 		},
+		keymaps = {
+			["g?"] = "actions.show_help",
+			["<CR>"] = "actions.select",
+			["<C-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" },
+			["<C-h>"] = {
+				"actions.select",
+				opts = { horizontal = true },
+				desc = "Open the entry in a horizontal split",
+			},
+			["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+			["<C-p>"] = "actions.preview",
+			["<C-c>"] = "actions.close",
+			["<C-l>"] = "actions.refresh",
+			["-"] = "actions.parent",
+			["_"] = "actions.open_cwd",
+			["`"] = "actions.cd",
+			["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory", mode = "n" },
+			["gs"] = "actions.change_sort",
+			["gx"] = "actions.open_external",
+			["g."] = "actions.toggle_hidden",
+			["g\\"] = "actions.toggle_trash",
+		},
 	},
 	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
