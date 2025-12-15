ad3efedb924717f012ee696b3a50873761250fc9
tofu345 <shammahayotunde1@gmail.com>
2025-12-15 20:44:45 +0000

    bye lir, hi nvim-tree

--- lua/tofu345/lazy/screen-key.lua
+++ lua/tofu345/lazy/screen-key.lua
@@ -2,11 +2,9 @@ return {
 	"NStefan002/screenkey.nvim",
 	lazy = false,
 	version = "*", -- or branch = "main", to use the latest commit
-	config = function()
-		require("screenkey").setup({
-			win_opts = {
-				border = "rounded",
-			},
-		})
-	end,
+	opts = {
+		win_opts = {
+			border = "rounded",
+		},
+	},
 }
