5788d18058d43d995bf8163dfed8df1391d0498c
tofu345 <shammahayotunde1@gmail.com>
2026-01-29 14:17:30 +0000

    all in on vague!

--- lua/tofu345/lazy/colors.lua
+++ lua/tofu345/lazy/colors.lua
@@ -1,38 +1,8 @@
 return {
-	"rktjmp/lush.nvim",
-	name = "colors",
+	"vague-theme/vague.nvim",
 	lazy = false,
 	priority = 1000, -- make sure to load this before all the other plugins
-	dependencies = {
-		"vague-theme/vague.nvim",
-		"rose-pine/neovim",
-		"Shatur/neovim-ayu",
-		"metalelf0/jellybeans-nvim",
-	},
 	config = function()
-		require("rose-pine").setup({
-			variant = "main",
-			dim_inactive_windows = false,
-			extend_background_behind_borders = true,
-			enable = {
-				migrations = true,
-				terminal = true,
-				legacy_highlights = true,
-			},
-			styles = {
-				bold = true,
-				italic = false,
-				transparency = true,
-			},
-			highlight_groups = {
-				Comment = { italic = false },
-				CursorLine = { bg = "base" },
-				TreesitterContext = { bg = "base" },
-				TreesitterContextLineNumber = { bg = "base" },
-			},
-			groups = {},
-		})
-
 		require("vague").setup({
 			transparent = true,
 			bold = true,
@@ -60,14 +30,10 @@ return {
 				hl.TabLine = { fg = colors.comment, bg = black }
 				hl.TabLineFill.bg = black
 
-				vim.fn.sign_define('DapStopped', {text='→', texthl='', linehl='', numhl=''})
+				vim.fn.sign_define("DapStopped", { text = "→", texthl = "", linehl = "", numhl = "" })
 			end,
 		})
 
 		vim.cmd([[colorscheme vague]])
-
-		-- vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#131416" })
-		-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	end,
 }
